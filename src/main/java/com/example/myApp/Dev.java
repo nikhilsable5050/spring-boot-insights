df46b6835df32b853105d4669cfc1f7640810b3a
nikhisable5050 <nikhilsable5050@gmail.com>
2025-10-10 16:45:46 +0530

    Initial commit

---
+++ src/main/java/com/example/myApp/Dev.java
@@ -0,0 +1,10 @@
+package com.example.myApp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+    public void build(){
+        System.out.println("Working on awesome project");
+    }
+}
