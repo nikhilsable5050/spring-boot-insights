42355b6078c0644979bb1b5f5937e52abc96c68c
nikhisable5050 <nikhilsable5050@gmail.com>
2026-01-07 16:51:51 +0530

    Structured MVC

---
+++ Spring Boot Layered Architecture/Spring-Boot-Layered-Architecture/src/main/java/com/nikhilsable/Spring/Boot/Layered/Architecture/SpringBootLayeredArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.nikhilsable.Spring.Boot.Layered.Architecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLayeredArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLayeredArchitectureApplication.class, args);
+	}
+
+}
---
+++ Spring Boot Layered Architecture/Spring-Boot-Layered-Architecture/src/test/java/com/nikhilsable/Spring/Boot/Layered/Architecture/SpringBootLayeredArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikhilsable.Spring.Boot.Layered.Architecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLayeredArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
