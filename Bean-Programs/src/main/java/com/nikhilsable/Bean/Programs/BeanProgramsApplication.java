b5a371ee98c6a199067cec909c83b2bd7ca920df
nikhisable5050 <nikhilsable5050@gmail.com>
2026-01-08 20:30:44 +0530

    Add Spring Bean programs examples

---
+++ Bean-Programs/src/main/java/com/nikhilsable/Bean/Programs/BeanProgramsApplication.java
@@ -0,0 +1,13 @@
+package com.nikhilsable.Bean.Programs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanProgramsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanProgramsApplication.class, args);
+	}
+
+}
---
+++ Bean-Programs/src/test/java/com/nikhilsable/Bean/Programs/BeanProgramsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikhilsable.Bean.Programs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanProgramsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
