522f77599e20ed78edea08e763a1154110a42f58
nikhisable5050 <nikhilsable5050@gmail.com>
2026-01-08 20:29:40 +0530

    Basic Structure

---
+++ Spring Boot Layered Architecture/Spring-Boot-Layered-Architecture/src/main/java/com/nikhilsable/Spring/Boot/Layered/Architecture/controllers/EmployeeController.java
@@ -0,0 +1,15 @@
+package com.nikhilsable.Spring.Boot.Layered.Architecture.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+    @GetMapping(path = "/getSecretMessage")
+    public String getMySuperSecretMessage(){
+        return "Secret message: 2dgFI*$#H87yg";
+    }
+
+
+}
---
+++ Spring Boot Layered Architecture/Spring-Boot-Layered-Architecture/src/main/java/com/nikhilsable/Spring/Boot/Layered/Architecture/dto/EmployeeDTO.java
@@ -0,0 +1,13 @@
+package com.nikhilsable.Spring.Boot.Layered.Architecture.dto;
+
+import java.time.LocalDate;
+
+public class EmployeeDTO {
+    Long id;
+    String name;
+    String email;
+    Integer age;
+    LocalDate dateOfJoining;
+    Boolean isActive;
+
+}
