40d9cee50a9f64ddcf3049744bad70046bdcc9b3
nikhisable5050 <nikhilsable5050@gmail.com>
2026-01-11 22:21:03 +0530

    Add EmployeeController with GET employee by id API

--- layered-architecture/Spring-Boot-Layered-Architecture/src/main/java/com/nikhilsable/Spring/Boot/Layered/Architecture/controllers/EmployeeController.java
+++ layered-architecture/Spring-Boot-Layered-Architecture/src/main/java/com/nikhilsable/Spring/Boot/Layered/Architecture/controllers/EmployeeController.java
@@ -1,15 +1,25 @@
 package com.nikhilsable.Spring.Boot.Layered.Architecture.controllers;
 
 
+import com.nikhilsable.Spring.Boot.Layered.Architecture.dto.EmployeeDTO;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
 
 @RestController
 public class EmployeeController {
-    @GetMapping(path = "/getSecretMessage")
-    public String getMySuperSecretMessage(){
-        return "Secret message: 2dgFI*$#H87yg";
+
+//    @GetMapping(path = "/getSecretMessage")
+//    public String getMySuperSecretMessage(){
+//        return "Secret message: 2dgFI*$#H87yg";
+//    }
+
+
+    @GetMapping(path = "/employees/{employeeId}")
+    public EmployeeDTO getEmployeeById (@PathVariable Long employeeId){
+        return new EmployeeDTO(employeeId, "nikhil" ,"niks@gmail.com",21, LocalDate.of(2026,1,2),true);
+
     }
-
-
 }
