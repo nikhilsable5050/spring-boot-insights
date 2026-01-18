40e53ae920c34c241cb5856f971b91cee30d0b3d
nikhisable5050 <nikhilsable5050@gmail.com>
2026-01-18 22:20:08 +0530

    Add spring boot annotations module

---
+++ annotation-mastery/src/main/java/com/example/annotationmastery/AnnotationMasteryApplication.java
@@ -0,0 +1,13 @@
+package com.example.annotationmastery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnotationMasteryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnotationMasteryApplication.class, args);
+	}
+
+}
---
+++ annotation-mastery/src/test/java/com/example/annotationmastery/AnnotationMasteryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.annotationmastery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnotationMasteryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
