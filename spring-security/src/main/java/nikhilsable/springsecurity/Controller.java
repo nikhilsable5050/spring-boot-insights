d1b996438570ce0343b48e2a381468527447979e
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-04-07 19:04:38 +0530

    Add role based auth

--- spring-security/src/main/java/nikhilsable/springsecurity/Controller.java
+++ spring-security/src/main/java/nikhilsable/springsecurity/Controller.java
@@ -1,21 +1,26 @@
 package nikhilsable.springsecurity;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
-    @GetMapping("/hello")
-    public String hello(){
-        return "Hello world";
-    }
-    @GetMapping("/hey")
-    public String hey(){
-        return "Hey there";
-    }
     @GetMapping("hi")
     public String hii(){
         return "Hi";
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user")
+    public String userEndpoint(){
+        return "Hello, user";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String adminEndpoint(){
+        return "Hello, admin";
     }
 }
