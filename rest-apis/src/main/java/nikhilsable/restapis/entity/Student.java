58abff763933dc7fe847603ce38e07204933c9d5
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-08-26 20:26:05 +0530

    Add student entity

---
+++ rest-apis/src/main/java/nikhilsable/restapis/entity/Student.java
@@ -0,0 +1,23 @@
+package nikhilsable.restapis.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "students")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private int age;
+    private String course;
+}
