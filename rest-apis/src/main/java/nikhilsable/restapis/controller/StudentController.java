faaec555fb3b0a0c18e722c3113468f07b31bf1a
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-08-28 10:51:47 +0530

    Add student end point

--- rest-apis/src/main/java/nikhilsable/restapis/controller/StudentController.java
+++ rest-apis/src/main/java/nikhilsable/restapis/controller/StudentController.java
@@ -1,4 +1,23 @@
 package nikhilsable.restapis.controller;
 
+import lombok.RequiredArgsConstructor;
+import nikhilsable.restapis.entity.Student;
+import nikhilsable.restapis.service.StudentServiceImpl;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/students")
 public class StudentController {
+
+    private final StudentServiceImpl studentService;
+
+    @PostMapping
+    public Student createStudent(@RequestBody Student student) {
+        return studentService.creatStudent(student);
+    }
+
 }
